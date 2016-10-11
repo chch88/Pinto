ab38d564efa98eacf6b94e2b054261a2cf8d62f7
Chudant <ch-ch-@hotmail.fr>
2016-10-11 15:00:38 +0200

    liens js et icon

--- page/header.php
+++ page/header.php
@@ -6,24 +6,30 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
     <title>PINTO HAPPY HOUR</title>
 
-    <script type="text/javascript" src="/Pinto/materialize/materialize.js"></script>
-    <script type="text/javascript" src="/Pinto/scripts.js"></script>
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="/Pinto/materialize/materialize.css">
     <link rel="stylesheet" type="text/css" href="/Pinto/feuille.css">
+
 
 </head>
 
 <body>
+
+<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script type="text/javascript" src="/Pinto/materialize/materialize.js"></script>
+<script type="text/javascript" src="/Pinto/scripts.js"></script>
 
 <nav>
     <div class="nav-wrapper">
         <a href="/Pinto/index.php" class="brand-logo center">PINTO HAPPY HOUR</a>
         <ul class="right hide-on-med-and-down">
             <li><a class="waves-effect waves-light btn">Inscription</a></li>
-            <li><a class="waves-effect waves-light btn"  href="page/page_connexion.php">Connexion</a></li>
+            <li><a class="waves-effect waves-light btn"  href="/Pinto/page/page_connexion.php">Connexion</a></li>
         </ul>
     </div>
 </nav>
--- page/page_connexion.php
+++ page/page_connexion.php
@@ -2,22 +2,39 @@
 
 <?php include("header.php"); ?>
 
-    <div class="row">
-        <form class="col s12">
-            <div class="row">
-                <div class="input-field col s6">
-                    <i class="material-icons prefix">account_circle</i>
-                    <input id="icon_prefix" type="text" class="validate">
-                    <label for="icon_prefix">First Name</label>
-                </div>
-                <div class="input-field col s6">
-                    <i class="material-icons prefix">phone</i>
-                    <input id="icon_telephone" type="tel" class="validate">
-                    <label for="icon_telephone">Telephone</label>
-                </div>
-            </div>
-        </form>
-    </div>
+<div class="container">
 
+    <br>
+    
+    <h3 class="center-align">CONNECTION</h3>
+
+    <div class="row">
+        <div class="input-field col s4 offset-s4">
+            <i class="material-icons prefix">account_circle</i>
+            <input id="icon_prefix" type="text" class="validate">
+            <label for="icon_prefix" class="">Identifiant</label>
+        </div>
+    </div> <!-- fin row -->
+
+    <div class="row">
+        <div class="input-field col s4 offset-s4">
+            <i class="material-icons prefix">vpn_key</i>
+            <input id="password" type="password" class="validate">
+            <label for="password">Mot de passe</label>
+        </div>
+    </div><!-- fin row -->
+
+    <br>
+    <br>
+
+<div class="center">
+    <a class="waves-effect waves-light btn" type="submit" name="action">Envoyer</a>
+</div>
+
+    <br>
+    <br>
+
+
+</div> <!-- fin container -->
 
 <?php include("footer.php"); ?>
